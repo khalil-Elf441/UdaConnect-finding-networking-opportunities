8fc98011c2105082f52e3a6562a40a2b013e2b93
khalil-Elf441 <khalil.el-fallahi@alumni.univ-avignon.fr>
2022-11-06 12:33:37 +0100

    Kafka producer/coonsumer send/receive data - OK

--- modules/LocationService/app/udaconnect/services.py
+++ modules/LocationService/app/udaconnect/services.py
@@ -8,12 +8,14 @@ from app.udaconnect.models import Connection, Location, Person
 from app.udaconnect.schemas import ConnectionSchema, LocationSchema, PersonSchema
 from geoalchemy2.functions import ST_AsText, ST_Point
 from sqlalchemy.sql import text
+from datetime import datetime
 
 from kafka import KafkaProducer
 
 TOPIC_NAME = 'locations'
 KAFKA_SERVER = '127.0.0.1:9092'
 
+DATE_FORMAT = "%Y-%m-%d"
 
 producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
 
@@ -41,17 +43,24 @@ class LocationService:
         # if validation_results:
         #     logger.warning(f"Unexpected data format in payload: {validation_results}")
         #     raise Exception(f"Invalid payload: {validation_results}")
+        # creation_time : SELECT FORMAT(GetDate(),'yyyy-mm-dd')
+        
 
         new_location = {
             "person_id": location["person_id"],
-            "coordinate_latitude": location["latitude"],
-            "coordinate_longitude": location["longitude"]
+            "latitude": location["latitude"],
+            "longitude": location["longitude"],
+            "creation_time": str(location["creation_time"])
             }
-        location_encode_data = json.dumps(new_location, indent=2).encode('utf-8')
-        producer.send(TOPIC_NAME, location_encode_data)
-        producer.flush()
+            
+        try:    
+            location_encode_data = json.dumps(new_location, indent=2).encode('utf-8')
+            producer.send(TOPIC_NAME, location_encode_data)
+            producer.flush()
+            return {"OK"}
+        except :
+            return {"ERROR"}
 
-        return new_location
 
     @staticmethod
     def retrieve_person_datediff(person_id: int, start_date: datetime, end_date: datetime) -> Location:
