1c7d4dcfd7676610c0b0295a0749ca88d190ec45
khalil-Elf441 <khalil.el-fallahi@alumni.univ-avignon.fr>
2022-11-19 15:16:16 +0100

    Fix PS create - Key (id)=(1) already exists

--- modules/PersonService/app/udaconnect/services.py
+++ modules/PersonService/app/udaconnect/services.py
@@ -126,6 +126,7 @@ class PersonService:
     @staticmethod
     def create(person: Dict) -> Person:
         new_person = Person()
+        new_person.id = int(person["id"])
         new_person.first_name = person["first_name"]
         new_person.last_name = person["last_name"]
         new_person.company_name = person["company_name"]
